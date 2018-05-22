f16a29c2e1ab71265bf1add98078a475c81a0219
Cristobal Grana <cristobal.wee@gmail.com>
2018-05-22 14:07:42 -0500

    Added animations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles.css';
 
 import logo from './media/logo.svg';
 import line from './media/line.svg';
+import Project from './components/project.js';
 
 class App extends Component {
   constructor(props) {
@@ -34,16 +35,64 @@ class App extends Component {
     });
   }
 
+  tweenUp(targets, delay) {
+    targets.map((target, i) => {
+      anime({
+        targets: target,
+        top: -20,
+        opacity: 0,
+        duration: 400,
+        easing: 'easeInBack',
+        offset: (i * 100),
+        delay: delay
+      });
+    });
+  }
+
+  tweenDown(targets, delay) {
+    targets = targets.reverse();
+    targets.map((target, i) => {
+      anime({
+        targets: target,
+        top: 20,
+        opacity: 0,
+        duration: 400,
+        easing: 'easeInBack',
+        offset: (i * 100),
+        delay: delay
+      });
+    });
+  }
+
+  tweenIn(targets, delay) {
+    targets.map((target, i) => {
+      anime({
+        targets: target,
+        top: [20, 0],
+        opacity: [0, 1],
+        duration: 400,
+        easing: 'easeOutBack',
+        offset: (i * 100),
+        delay: delay
+      });
+    });
+  }
+
   scroll(flag) {
+    let position = this.state.currPos;
+
     if (flag) {
       $("span").removeClass("glitch");
-      anime({
-        targets: '.fadeup',
-        top: -50,
-        duration: 1000
-      });
+      let tl = anime.timeline({
+        loop: false
+      })
+      this.tweenUp(['#landing-head', '#landing-subhead', '#landing-subhead-2'], 0);
+      $('#telescope').css('opacity', 1);
+      this.tweenIn(['#telescope-number', '#telescope-head', '#telescope-color'], 800);
     } else {
-      $("span").addClass("glitch");
+      if (this.state.currPos === 1) {
+        $("span").addClass("glitch");
+      }
     }
   }
 
@@ -57,11 +106,14 @@ class App extends Component {
           <div id="landing-content" className="content">
             <span id="landing-head" className="glitch block fadeup" data-text="Hello there,">Hello there,</span>
             <span id="landing-subhead" className="glitch block fadeup" data-text="My name is Cristobal Graña, and I’m a">My name is Cristobal Graña, and I’m a</span>
-            <span id="landing-subhead" className="glitch block fadeup" data-text="student at UIUC">student at UIUC</span>
+            <span id="landing-subhead-2" className="glitch block fadeup" data-text="student at UIUC">student at UIUC</span>
           </div>
-          <div className="scroll">
-            <p>scroll</p>
-            <img src={line}></img>
+        </div>
+        <div id="telescope" className="project">
+          <div id="telescope-content" className="content">
+            <p id="telescope-number" className="subhead">01/04</p>
+            <p id="telescope-head" className="head">Telescope</p>
+            <div id="telescope-color"></div>
           </div>
         </div>
       </div>
---
+++ src/components/project.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import anime from 'animejs';
+import '../styles.css';
+
+class Project extends Component {
+  render() {
+    return (
+      <div>Hello there</div>
+    )
+  }
+}
+
+export default Project;
